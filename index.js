de378a1282ca5a7c5798c32a3894dc5193322bec
Catevika <dominique.bello@outlook.fr>
2023-10-17 16:43:06 +0200

    Update the path to frontend in index

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use(express.json());
 app.use('/api/', commentRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../interactive-comments-react/dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../interactive-comments-react', 'dist', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
